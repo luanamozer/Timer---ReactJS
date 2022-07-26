fa552c7a1600c0779c4a90f935f802778ac98214
Luana Mozer <luanamozzer1996@gmail.com>
2022-07-26 10:20:46 -0300

    criando definição ts styles components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
+  background-color: ${(props) => props.theme.primary};
+
+  /* 
   ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
-  }}
+  }} */
 `;
