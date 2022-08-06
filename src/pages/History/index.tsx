e76e056a008ba7bc86b1a4a705168e9f4bf861c1
Luana Mozer <luanamozzer1996@gmail.com>
2022-08-06 17:43:40 -0300

    estilização page history e component status

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,69 @@
+import { HistoryConatiner, HistoryList, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryConatiner>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Concluido</Status>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 min</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="red">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryConatiner>
+  );
+}
